453b92cad061304c883c9c66fca3a516a5741766
KasidisGit <45381468+KasidisGit@users.noreply.github.com>
2019-04-05 15:28:15 +0700

    Update mol_project.py

--- mol_project.py
+++ mol_project.py
@@ -1,5 +1,6 @@
 import arcade.key
 import random
+import arcade
 
 MOVEMENT_SPEED = 4
 DIR_STILL = 0
@@ -20,6 +21,13 @@ KEY_MAP = {arcade.key.UP: DIR_UP,
            arcade.key.RIGHT: DIR_RIGHT, }
 
 
+class Background:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+
+
 class Boat:
 
     def __init__(self, world, x, y):
@@ -36,33 +44,102 @@ class Boat:
         self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
 
 
+class Ball:
+    def __init__(self, position_x, position_y, radius, color):
+        # Take the parameters of the init function above, and create instance variables out of them.
+        self.position_x = position_x
+        self.position_y = position_y
+        self.radius = radius
+        self.color = color
+
+    def draw(self):
+        """ Draw the balls with the instance variables we have. """
+        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
+
+
 class Worm:
     def __init__(self, world, x, y):
         self.world = world
-        self.x = x
-        self.y = y
-        self.direction = DIR_STILL
-        self.next_direction = DIR_STILL
+        self.x = self.world.boat.x
+        self.y = self.world.boat.y
+        self.vy = 5
+        self.space = False
         self.score = 0
 
-    def move(self, direction):
-        self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
-        self.y += MOVEMENT_SPEED * DIR_OFFSETS[direction][1]
-
     def update(self, delta):
-        pass
+        self.move_down()
+        if self.space:
+            if self.world.fish1.x - 100 <= self.x <= self.world.fish1.x + 100 and self.world.fish1.y - 75 <= self.y <= self.world.fish1.y + 75:
+                self.world.fish1.is_caught = True
+                self.space = False
+                self.move_up()
+
+    def move_up(self):
+        if self.space and not self.world.fish1.is_caught and self.space:
+            self.y += self.vy
+
+    def move_down(self):
+        if self.space:
+            self.y -= self.vy
+
+    def back(self):
+        self.x = self.world.boat.x
+        self.y = self.world.boat.y
 
 
-class Fish:
+class Fish1:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
-        self.is_catched = False
+        self.vx = 5
+        self.vy = 5
+        self.stat = True
+        self.is_caught = False
 
     def update(self):
-        if not self.is_catched:
-            self.x += 2
+        if self.stat:
+            self.x += self.vx
+
+    def catch(self):
+        if self.is_caught:
+            self.vx = 0
+            self.world.worm.y += self.vy
+            self.y += self.vy
+            if self.y >= self.world.boat.y - 55:
+                self.stat = False
+                self.back()
+
+    def back(self):
+        self.stat = True
+        self.x = -200
+        self.y = 40
+        self.vx = 2
+        arcade.draw_circle_filled(self.world.worm.x + 73, self.world.worm.y - 55, 1.3, arcade.color.BLACK)
+
+
+class Fish3:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.is_caught = False
+
+    def update(self):
+        if not self.is_caught:
+            self.x -= 2
+
+
+class Fish4:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.is_caught = False
+
+    def update(self):
+        if not self.is_caught:
+            self.x += 10
 
 
 class World:
@@ -73,10 +150,13 @@ class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.background = Background(self, 700, 400)
         self.boat = Boat(self, 700, 600)
-        self.worm = Worm(self, 60, 100)
+        self.worm = Worm(self, 700, 600)
         self.fish_list = []
-        self.fish = Fish(self, 0, random.randint(20, 500))
+        self.fish1 = Fish1(self, -150, random.randint(20, 500))
+        self.fish3 = Fish3(self, 1550, random.randint(20, 500))
+        self.fish4 = Fish4(self, -150, random.randint(20, 500))
         self.state = World.STATE_FROZEN
         self.next_direction = DIR_STILL
 
@@ -84,7 +164,11 @@ class World:
         if self.state in [World.STATE_FROZEN, World.STATE_DEAD]:
             return
         self.boat.update(delta)
-        self.fish.update()
+        self.worm.update(delta)
+        self.fish1.update()
+        self.fish1.catch()
+        self.fish3.update()
+        self.fish4.update()
 
     def start(self):
         self.state = World.STATE_STARTED
@@ -95,3 +179,5 @@ class World:
     def on_key_press(self, key, key_modifiers):
         if key in KEY_MAP:
             self.boat.next_direction = KEY_MAP[key]
+        if key == arcade.key.SPACE:
+            self.worm.space = True
