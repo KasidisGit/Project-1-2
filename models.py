b0d0e1f5aaf477932e544f827bca88c0c26f0b87
KasidisGit <45381468+KasidisGit@users.noreply.github.com>
2019-05-16 09:32:47 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,244 +1,115 @@
-import arcade.key
-import random
-import arcade
+from models import *
 
-MOVEMENT_SPEED = 4
-DIR_STILL = 0
-DIR_UP = 1
-DIR_RIGHT = 2
-DIR_DOWN = 3
-DIR_LEFT = 4
-
-DIR_OFFSETS = {DIR_STILL: (0, 0),
-               DIR_UP: (0, 1),
-               DIR_RIGHT: (1, 0),
-               DIR_DOWN: (0, -1),
-               DIR_LEFT: (-1, 0)}
-
-KEY_MAP = {arcade.key.UP: DIR_UP,
-           arcade.key.DOWN: DIR_DOWN,
-           arcade.key.LEFT: DIR_LEFT,
-           arcade.key.RIGHT: DIR_RIGHT, }
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 800
 
 
-class Background:
-    def __init__(self, world, x, y):
-        self.world = world
-        self.x = x
-        self.y = y
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 
-class Boat:
-    def __init__(self, world, x, y):
-        self.world = world
-        self.direction = DIR_STILL
-        self.next_direction = DIR_STILL
-        self.x = x
-        self.y = y
-
-    def update(self, delta):
-        if self.world.worm.space is not True:
-            self.move(self.next_direction)
-
-    def move(self, direction):
-        if self.x >= -150:
-            self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
-            if self.x >= 1550:
-                self.x = 0
-        else:
-            self.x = 1400
-
-
-class Worm:
-    WORM_SPEED = 10
-
-    def __init__(self, world, x, y):
-        self.world = world
-        self.x = self.world.boat.x
-        self.y = self.world.boat.y
-        self.space = False
-        self.stat = True
-        self.boat_head = "R"
-        self.vy = 0
-        self.score = 0
-        self.is_hooked = False
-
-    def update(self, delta):
-        if self.is_hooked is False and self.boat_head == "R":
-            self.x = self.world.boat.x
-            self.y = self.world.boat.y
-        elif self.is_hooked is False and self.boat_head == "L":
-            self.x = self.world.boat.x - 73
-            self.y = self.world.boat.y
-        if self.stat:
-            if self.y > 0 and self.vy <= 0:
-                self.move_down()
-            else:
-                self.back()
-                self.move_up()
-        else:
-            self.back()
-            self.move_up()
-
-    def hooking(self):
-        if self.space is not True:
-            self.is_hooked = True
-            self.world.boat.direction = DIR_STILL
-            self.vy = - Worm.WORM_SPEED
-            self.space = True
-
-    def back(self):
-        self.vy = Worm.WORM_SPEED
-
-    def move_up(self):
-        if self.y < 600:
-            self.y += self.vy
-        else:
-            self.y = 600
-            self.vy = 0
-            self.space = False
-            self.stat = True
-            self.is_hooked = False
-
-    def move_down(self):
-        self.y += self.vy
-
-
-class Fish1:
-    def __init__(self, world, x, y):
-        self.world = world
-        self.x = x
-        self.y = y
-        self.vx = 5
-        self.vy = 5
-        self.stat = True
-        self.is_caught = False
-        self.score = 300
-
-    def update(self):
-        if self.stat:
-            self.x += self.vx
-
-    def hit(self):
-        if self.x - 50 <= self.world.worm.x <= self.x + 50 and self.y - 75 <= self.world.worm.y <= self.y + 75:
-            self.is_caught = True
-            self.world.worm.stat = False
-
-    def catch(self):
-        if self.is_caught:
-            # World.all_score += self.score
-            self.vx = 0
-            self.y += self.vy
-            if self.y >= self.world.boat.y - 55:
-                self.stat = False
-
-    def back(self):
-        pass
-
-
-class Fish2:
-    def __init__(self, world, x, y):
-        self.world = world
-        self.x = x
-        self.y = y
-        self.vx = 5
-        self.vy = 5
-        self.stat = True
-        self.is_caught = False
-        self.score = 100
-
-    def update(self):
-        if self.stat:
-            self.x += self.vx
-
-    def hit(self):
-        if self.x - 50 <= self.world.worm.x <= self.x + 50 and self.y - 75 <= self.world.worm.y <= self.y + 75:
-            self.is_caught = True
-            self.world.worm.stat = False
-
-    def catch(self):
-        if self.is_caught:
-            # World.all_score += self.score
-            self.vx = 0
-            self.y += self.vy
-            if self.y >= self.world.boat.y - 55:
-                self.stat = False
-
-
-class Fish3:
-    def __init__(self, world, x, y):
-        self.world = world
-        self.x = x
-        self.y = y
-        self.vx = 5
-        self.vy = 5
-        self.stat = True
-        self.is_caught = False
-        self.score = 1000
-
-    def update(self):
-        if self.stat:
-            self.x += self.vx
-
-    def hit(self):
-        if self.x - 150 <= self.world.worm.x <= self.x + 150 and self.y - 75 <= self.world.worm.y <= self.y + 75:
-            self.is_caught = True
-            self.world.worm.stat = False
-
-    def catch(self):
-        if self.is_caught:
-            self.vx = 0
-            self.y += self.vy
-            if self.y >= self.world.boat.y - 55:
-                self.stat = False
-
-
-class World:
-    STATE_FROZEN = 1
-    STATE_STARTED = 2
-    STATE_DEAD = 3
-
+class Window(arcade.Window):
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.background = Background(self, 700, 400)
-        self.boat = Boat(self, 700, 600)
-        self.worm = Worm(self, 700, 600)
-        self.all_score = 0
-        self.fish1 = Fish1(self, -550, random.randint(200, 500))
-        self.fish2 = Fish2(self, -150, random.randint(200, 500))
-        self.fish3 = Fish3(self, -950, random.randint(200, 500))
-        self.state = World.STATE_FROZEN
-        self.next_direction = DIR_STILL
+        super().__init__(width, height)
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.background_1 = ModelSprite("BG2.jpg", model=self.world.background_1)
+        self.background_2 = ModelSprite("BG2 Dark.jpg", model=self.world.background_2)
+        self.background_3 = ModelSprite("start text.png", model=self.world.background_3)
+        self.background_4 = ModelSprite("Gameover.png", model=self.world.background_4)
+        self.boat_sprite_l = ModelSprite("boatL.png", model=self.world.boat)
+        self.boat_sprite_r = ModelSprite("boatR.png", model=self.world.boat)
+        self.fish_sprite_1 = ModelSprite("fish1.png", model=self.world.fish1)
+        self.fish_sprite_2 = ModelSprite("fish7.png", model=self.world.fish2)
+        self.fish_sprite_3 = ModelSprite("fish4.png", model=self.world.fish3)
+        self.fish_sprite_4 = ModelSprite("fish5.png", model=self.world.fish4)
+        self.fish_sprite_5 = ModelSprite("fish3.png", model=self.world.fish5)
+        self.bone_sprite = ModelSprite("boney.png", model=self.world.bone)
+        self.bone_sprite_r = ModelSprite("bone.png", model=self.world.bone2)
 
     def update(self, delta):
-        if self.state in [World.STATE_FROZEN, World.STATE_DEAD]:
-            return
-        self.boat.update(delta)
-        self.worm.update(delta)
-        self.fish1.update()
-        self.fish1.hit()
-        self.fish1.catch()
-        self.fish2.update()
-        self.fish2.hit()
-        self.fish2.catch()
-        self.fish3.update()
-        self.fish3.hit()
-        self.fish3.catch()
+        if self.world.time > 900 and self.world.state == 2:
+            self.world.time -= 0.5
+        if self.world.time > 1300:
+            self.world.time = 1300
+        self.world.update(delta)
 
-    def start(self):
-        self.state = World.STATE_STARTED
+    def on_draw(self):
+        arcade.start_render()
 
-    def is_started(self):
-        return self.state == World.STATE_STARTED
+        if self.world.time <= 900:
+            self.world.died()
+            self.background_2.draw()
+            self.background_4.draw()
+            arcade.draw_text("High Score : " + str(self.world.all_score), SCREEN_WIDTH / 2, 150, arcade.color.BLACK, 35,
+                             2000, align="center", anchor_x="center", anchor_y="center")
+
+        if self.world.state == 2:
+            self.background_1.draw()
+            if self.world.worm.y >= 0:
+                if self.world.boat.next_direction == 2:
+                    self.boat_sprite_r.draw()
+                    arcade.draw_circle_filled(self.world.worm.x + 73, self.world.worm.y - 55, 7, arcade.color.BLACK)
+                elif self.world.boat.next_direction == 4:
+                    self.boat_sprite_l.draw()
+                    arcade.draw_circle_filled(self.world.worm.x - 73, self.world.worm.y - 55, 7, arcade.color.BLACK)
+                else:
+                    self.boat_sprite_r.draw()
+                    arcade.draw_circle_filled(self.world.worm.x + 73, self.world.worm.y - 55, 7, arcade.color.BLACK)
+            if self.world.fish1.stat:
+                self.fish_sprite_1.draw()
+            if self.world.fish2.stat:
+                self.fish_sprite_2.draw()
+            if self.world.fish3.stat:
+                self.fish_sprite_3.draw()
+            if self.world.fish4.stat:
+                self.fish_sprite_4.draw()
+            if self.world.fish5.stat:
+                self.fish_sprite_5.draw()
+            if self.world.bone.stat:
+                self.bone_sprite.draw()
+            if self.world.bone2.stat:
+                self.bone_sprite_r.draw()
+
+            arcade.draw_rectangle_outline(1100, 750.5, 400, 23.5, arcade.color.WHITE, border_width=5)
+            arcade.draw_line(900, 757.5, self.world.time, 757.5, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 755, self.world.time, 755, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 752.5, self.world.time, 752.5, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 750, self.world.time, 750, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 747.5, self.world.time, 747.5, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 745, self.world.time, 745, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 742.5, self.world.time, 742.5, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 740, self.world.time, 740, arcade.color.RED, border_width=10)
+            arcade.draw_line(900, 760, self.world.time, 760, arcade.color.RED, border_width=10)
+            arcade.draw_text("Score : " + str(self.world.all_score), 50, 740, arcade.color.BLACK, 20, 2000)
+
+        if self.world.state == 1:
+            self.background_2.draw()
+            self.background_3.draw()
 
     def on_key_press(self, key, key_modifiers):
-        if key in KEY_MAP:
-            if self.worm.space is not True:
-                self.boat.next_direction = KEY_MAP[key]
-                if key == arcade.key.RIGHT:
-                    self.boat.boat_head = "R"
-                elif key == arcade.key.LEFT:
-                    self.boat.boat_head = "L"
-        if key == arcade.key.SPACE:
-            self.worm.hooking()
+        if not self.world.is_started() and self.world.state != 3:
+            self.world.start()
+        if self.world.state == 3 and key == arcade.key.ENTER:
+            self.world.reset()
+            self.world.state = 1
+        self.world.on_key_press(key, key_modifiers)
+
+
+def main():
+    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.set_window(window)
+    arcade.run()
+
+
+if __name__ == '__main__':
+    main()
